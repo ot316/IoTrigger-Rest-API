ee6828369cdfbab0d8a3814bcc9de22618e7b633
oli <olithompsson@rocketmail.com>
2021-06-28 16:42:18 +0100

    use json lists instead of block of text

--- controller.js
+++ controller.js
@@ -1,20 +1,27 @@
 exports.trimlist = function (req, res, trimlist) {
     body = '';
-
+    
     req.on('data', function (chunk) {
         body += chunk;
     });
-
+    
     req.on('end', function () {
         postBody = JSON.parse(body);
-        var line = trimlist.split(/\r?\n/);
-        var version = line[0].match(/\d+/)[0];
+        var lines = trimlist.split(/\r?\n/);
+        var version = lines[0].match(/\d+/)[0];
+        lines.splice(0,2);
         try {
             var clientVersion = postBody.version
             if (clientVersion < version) {
                 var response = {
                     "version": version,
-                    "trimlist": trimlist
+                    "trimlist": lines
+                }
+            }
+            else {
+                var response = {
+                    "version": version,
+                    "trimlist": null
                 }
             }
         } catch (error) {
@@ -22,7 +29,7 @@ exports.trimlist = function (req, res, trimlist) {
             console.error(response);
             console.error(error);
         }
-
+        
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(response));
